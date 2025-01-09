fa932f0d3286edfb1f3b46b8823f2f45a6dcdf60
Moahemd <dedo.abdelhamed@gmail.com>
2025-01-09 05:44:28 +0200

    Update web deployment files

--- android/app/src/main/kotlin/com/nazef/nazef/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nazef.nazef
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/nazef/nazeff/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nazef.nazeff
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
